0e69c40bd1165a9e93b4fdd5b728091cce38e19b
MaxFalkenberg <mff113@ic.ac.uk>
2022-11-02 16:58:27 +0000

    Updating pull script for news articles

--- pull_assets.py
+++ pull_assets.py
@@ -10,6 +10,7 @@ def Pull_Assets():
     repo.git.add('assets/data/livegraph_1500_cop27.js')
     repo.git.add('assets/data/livegraph_150_cop27_pro.js')
     repo.git.add('assets/data/livegraph_150_cop27_con.js')
+    repo.git.add('_news/*.md')
     try:
         repo.git.add('assets/data/trends_data.js')
     except:
